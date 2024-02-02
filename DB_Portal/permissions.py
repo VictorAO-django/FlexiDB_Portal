85fb1a71dd84b6781ab18236f49df32cb9b398fc
Olusola Ayomide <ayomideolusola5002@gmail.com>
2024-02-02 08:22:22 +0100

    Update permissions.py

--- DB_Portal/permissions.py
+++ DB_Portal/permissions.py
@@ -1,5 +1,8 @@
 from rest_framework.permissions import BasePermission
 from helper import get_client_ip
+from exceptions import *
+
+from core.models import Permission
 
 class IsVerified(BasePermission):
     message = "User must be verified"
@@ -12,3 +15,27 @@ class IpIsValid(BasePermission):
     def has_permission(self, request, view):
         ip = get_client_ip(request)
         return ip in request.user.ip_address
+
+class DatabasePermission:
+    def __init__(self, db_instance, user_instance, permission):
+        self.choices = ["readonly", "read-edit", "read-edit-create", "read-edit-create-delete"]
+        self.db_instance = db_instance
+        self.user_instance = user_instance
+        self.permission = permission
+        
+        try:
+            assert self.permission in self.choices , "Invalid Permission Type"
+        except AssertionError as err:
+            raise InvalidPermission()
+            
+    def check_permission(self):
+        try:
+            DbPermission = Permission.objects.get(user=user_instance, database=database_instance)
+            DbPermission = DbPermission.permission 
+            assert DbPermission == self.permission
+        except Permission.DoesNotExist:
+            raise NotPermitted()
+        except AssertionError:
+            raise NotPermitted()
+            
+        
