f0690069725398742dba274ba93fe8e646161518
Olusola Ayomide <ayomideolusola5002@gmail.com>
2024-02-05 22:10:48 +0100

    Update permissions.py

--- DB_Portal/permissions.py
+++ DB_Portal/permissions.py
@@ -17,32 +17,52 @@ class IpIsValid(BasePermission):
         return ip in request.user.ip_address
 
 class DatabasePermission:
-    def __init__(self, db_instance, user_instance, **kwargs):
-        self.choices = ["readonly", "read-edit", "read-edit-create", "read-edit-create-delete"]
+    def __init__(self, db_instance, user_instance, permission):
+        self.types = {
+            'read': ['read'],
+            'read-edit': ['read', 'read-edit'],
+            'read-edit-create': ['read', 'read-edit', 'read-edit-create'],
+            'read-edit-create-delete': ['read', 'read-edit', 'read-edit-create','read-edit-create-delete']
+        }
         self.db_instance = db_instance
         self.user_instance = user_instance
-        self.permission = kwargs.get('permission', None)
+        self.permission = permission
         
         try:
-            if self.permission:
-                assert self.permission in self.choices , "Invalid Permission Type"
+            assert self.permission in self.types , "Invalid Permission Type"
             
         except AssertionError as err:
             raise InvalidPermission()
             
     def check_permission(self):
         try:
+            assert db_instance.user == user_instance
+            return self.owner_permission()
+            
+        except AssertionError as err:
+            return self.user_permission()
+            
+    def user_permission(self):
+        try:
             #check if the user has permission to accss this database
             DbPermission = Permission.objects.get(user=self.user_instance, database=self.db_instance)
-            DbPermission = DbPermission.permission #get the user permission on this database
-            if self.permission: #if permission is provided
-                assert DbPermission == self.permission #ensure the user has the permission
+            user_permission = DbPermission.permission #get the user permission on this database
+            assert user_permission in self.types[user_permission], "no permission to perform this operation"
             pass
         
         except Permission.DoesNotExist:
             raise NotPermitted()
         
-        except AssertionError:
-            raise NotPermitted()
+        except AssertionError as err:
+            return Response({'detail': str(err)}, status=status.HTTP_403_FORBIDDEN)
+            
+    def owner_permission(self):
+        #owner has all permission 
+        pass
+
+
+
+            
+            
             
         
